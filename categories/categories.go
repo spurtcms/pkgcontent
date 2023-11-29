ceae7482423a7e11dfe8ca8ee84c3766817dcd7e
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2023-11-29 11:14:57 +0530

    category pkg  AllCategoriesWithSubList func added

--- categories/categories.go
+++ categories/categories.go
@@ -906,3 +906,126 @@ func (c Category) CheckCategroyGroupName(id int, name string) (bool, error) {
 	}
 	return true, nil
 }
+
+/*Get All cateogry with parents and subcategory*/
+func (c Category) AllCategoriesWithSubList() (arrangecategories []Arrangecategories, CategoryNames []string) {
+
+	var getallparentcat []TblCategory
+
+	C.GetAllParentCategory(&getallparentcat, c.Authority.DB)
+
+	var AllCategorieswithSubCategories []Arrangecategories
+
+	for _, Group := range getallparentcat {
+
+		GetData, _ := C.GetCategoryTree(Group.Id, c.Authority.DB)
+
+		var pid int
+
+		for _, categories := range GetData {
+
+			var addcat Arrangecategories
+
+			var individualid []CatgoriesOrd
+
+			pid = categories.ParentId
+
+		LOOP:
+			for _, GetParent := range GetData {
+
+				var indivi CatgoriesOrd
+
+				if pid == GetParent.Id {
+
+					pid = GetParent.ParentId
+
+					indivi.Id = GetParent.Id
+
+					indivi.Category = GetParent.CategoryName
+
+					individualid = append(individualid, indivi)
+
+					if pid != 0 {
+
+						goto LOOP
+
+					}
+				}
+
+			}
+
+			var ReverseOrder Arrangecategories
+
+			addcat.Categories = append(addcat.Categories, individualid...)
+
+			var singlecat []CatgoriesOrd
+
+			for i := len(addcat.Categories) - 1; i >= 0; i-- {
+
+				var Sing CatgoriesOrd
+
+				Sing.Id = addcat.Categories[i].Id
+
+				Sing.Category = addcat.Categories[i].Category
+
+				singlecat = append(singlecat, Sing)
+
+			}
+
+			var newcate CatgoriesOrd
+
+			newcate.Id = categories.Id
+
+			newcate.Category = categories.CategoryName
+
+			addcat.Categories = append(addcat.Categories, newcate)
+
+			singlecat = append(singlecat, newcate)
+
+			ReverseOrder.Categories = singlecat
+
+			AllCategorieswithSubCategories = append(AllCategorieswithSubCategories, ReverseOrder)
+		}
+
+	}
+
+	/*This for Channel category show also individual group*/
+	var FinalCategoryList []Arrangecategories
+
+	for _, val := range AllCategorieswithSubCategories {
+
+		if len(val.Categories) > 1 {
+
+			var infinalarray Arrangecategories
+
+			infinalarray.Categories = append(infinalarray.Categories, val.Categories...)
+
+			FinalCategoryList = append(FinalCategoryList, infinalarray)
+		}
+
+	}
+
+	var Categorynames []string
+
+	for _, val := range FinalCategoryList {
+
+		var name string
+
+		for index, cat := range val.Categories {
+
+			if len(val.Categories)-1 == index {
+
+				name += cat.Category
+			} else {
+				name += cat.Category + " / "
+			}
+
+		}
+
+		Categorynames = append(Categorynames, name)
+
+	}
+
+	return FinalCategoryList, Categorynames
+
+}
